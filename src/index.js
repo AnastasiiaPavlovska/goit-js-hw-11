2ea952eda08e9c6703f941e8800db834ec9076e8
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-07-30 12:32:42 +0200

    front: Add exports and REFS

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import NewApiService from './js/pixabay-api-service';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const REFS = {
+    form: document.querySelector('.search-form'),
+    boxImg: document.querySelector('.gallery'),
+    btn: document.querySelector('.load-more'),
+};
+
